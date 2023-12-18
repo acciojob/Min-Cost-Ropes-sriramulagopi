03e62a7efd5053fb400b156fafbb3475f9c7c355
sriramulagopi <129825285+sriramulagopi@users.noreply.github.com>
2023-12-18 19:03:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+    let mincost = 0;
+    let mincost2 = 0;
+    while(arr.length>1){
+        arr.sort((a,b)=>{
+            return a-b;
+        })
+        mincost=(arr[0]+arr[1]);
+        mincost2+=mincost;
+        arr.push(mincost);
+        arr.splice(0,2);
+
+    }
+    return mincost2;
   
 }
 
